0b2b7cffeceb9c903f85b2eba25c0bbf8ea9cea1
Timothy Mwangi <timomwa@gmail.com>
2015-05-02 10:20:22 +0300

    all

--- src/main/java/com/pixelandtag/sms/mt/workerthreads/SubscriptionBillingWorker.java
+++ src/main/java/com/pixelandtag/sms/mt/workerthreads/SubscriptionBillingWorker.java
@@ -188,6 +188,10 @@ public class SubscriptionBillingWorker implements Runnable {
 										}
 										
 									}else if(resp.toUpperCase().contains("Insufficient".toUpperCase())){
+										
+										//we'll try again
+										subscriptionejb.updateQueueStatus(1L, billable.getMsisdn(), Long.valueOf(billable.getService_id()));
+										
 										//Resume back to normal. No throttling
 										if(SubscriptionRenewal.isAdaptive_throttling()){
 											SubscriptionRenewal.setEnable_biller_random_throttling(false);
