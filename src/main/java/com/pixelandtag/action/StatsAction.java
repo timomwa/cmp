00e516559fb456afeccf539c83a6f0b3170fc678
Timothy Mwangi <timomwa@gmail.com>
2015-06-27 21:41:14 +0300

    Stats growing

--- src/main/java/com/pixelandtag/action/StatsAction.java
+++ src/main/java/com/pixelandtag/action/StatsAction.java
@@ -5,7 +5,11 @@ import java.math.BigInteger;
 import java.sql.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
@@ -26,8 +30,9 @@ public class StatsAction extends BaseActionBean {
 	private final String from_tz = "-04:00";
 	private final String to_tz  = "+03:00";
 	private Logger logger = Logger.getLogger(getClass());
-	
-SimpleDateFormat formatDayOfMonth  = new SimpleDateFormat("d");
+	private Random rand = new Random();
+
+	SimpleDateFormat formatDayOfMonth  = new SimpleDateFormat("d");
 	
 	public String convertToPrettyFormat(Date date){
 		int day = Integer.parseInt(formatDayOfMonth.format(date));
@@ -74,5 +79,106 @@ SimpleDateFormat formatDayOfMonth  = new SimpleDateFormat("d");
 		mainObject.put("labels", labels);
 		return sendResponse(mainObject.toString());
 	}
+	
+	
+	
+	@SuppressWarnings("unchecked")
+	public Resolution currentSubscriberDistribution() throws JSONException{
+		Query qry = cmp_dao.resource_bean.getEM().createNativeQuery("select  count(*) count, sms.service_name as 'service_name' from subscription sub left join sms_service sms on sms.id = sub.sms_service_id_fk where sub.subscription_status='confirmed' group by sms.service_name");
+		
+		List<Object[]> recs = qry.getResultList();
+		
+		JSONObject data = new JSONObject();
+		JSONArray dataArray = new JSONArray();
+		
+		
+		Queue<String> colorQueue = new PriorityQueue<String>();
+		colorQueue.add("#FDB45C");
+		colorQueue.add("#F7464A");
+		colorQueue.add("#46BFBD");
+		colorQueue.add("#886A08");
+		colorQueue.add("#CC2EFA");
+		colorQueue.add("#FF0040");
+		colorQueue.add("#04B4AE");
+		colorQueue.add("#DBA901");
+		colorQueue.add("#08298A");
+		colorQueue.add("#610B5E");
+		colorQueue.add("#74DF00");
+		colorQueue.add("#086A87");
+		colorQueue.add("#FDB10C");
+		colorQueue.add("#F7450A");
+		colorQueue.add("#46BFBD");
+		colorQueue.add("#870A08");
+		colorQueue.add("#CCCEFA");
+		colorQueue.add("#FF1140");
+		colorQueue.add("#04B4AE");
+		colorQueue.add("#DBA922");
+		colorQueue.add("#08298A");
+		colorQueue.add("#615B5E");
+		colorQueue.add("#74DF00");
+		colorQueue.add("#086A70");
+		
+		
+		Queue<String> highlightQueue = new PriorityQueue<String>();
+		highlightQueue.add("#FFC870");
+		highlightQueue.add("#FF5A5E");
+		highlightQueue.add("#5AD3D1");
+		highlightQueue.add("#F5DA81");
+		highlightQueue.add("#AC58FA");
+		highlightQueue.add("#FA5882");
+		highlightQueue.add("#01DFA5");
+		highlightQueue.add("#FACC2E");
+		highlightQueue.add("#013ADF");
+		highlightQueue.add("#B404AE");
+		highlightQueue.add("#80FF00");
+		highlightQueue.add("#00BFFF");
+		highlightQueue.add("#FFC860");
+		highlightQueue.add("#FF2A2E");
+		highlightQueue.add("#5AD0D0");
+		highlightQueue.add("#F5DA70");
+		highlightQueue.add("#AC40FA");
+		highlightQueue.add("#FA5702");
+		highlightQueue.add("#22DFA5");
+		highlightQueue.add("#FABB2E");
+		highlightQueue.add("#033ADF");
+		highlightQueue.add("#B440AE");
+		highlightQueue.add("#80FF22");
+		highlightQueue.add("#00BFEE");
+		
+		for(Object[] o : recs){
+			
+			BigInteger count = (BigInteger) o[0];
+			String service_name = (String) o[1];
+			
+			String colorHex = getRandomHexColor();
+			JSONObject dataPiece = new JSONObject();
+			dataPiece.put("value", count);
+			dataPiece.put("label", service_name);
+			dataPiece.put("color", "#"+colorHex);
+			dataPiece.put("highlight", "#"+incrementColor(colorHex,50));
+			
+			dataArray.put(dataPiece);
+			
+		}
+		data.put("data", dataArray);
+		
+		return sendResponse(data.toString());
+	}
 
+	
+	private String incrementColor(String hexVal, int increment) {
+		int value = Integer.parseInt(hexVal, 16);
+		value += increment;
+		return Integer.toHexString(value);
+	}
+	
+
+	private String getRandomHexColor() {
+	    char letters[] = "0123456789ABCDEF".toCharArray();
+	    String color = "";
+	    for (int i = 0; i < 6; i++ ) {
+	        color += letters[(rand.nextInt((15 - 0) + 1) + 0)];
+	    }
+	    return color;
+	}
 }
--- src/main/webapp/js/livebilling_graph.js
+++ src/main/webapp/js/livebilling_graph.js
@@ -33,6 +33,42 @@ function populate(){
 		
 		var x = new Ext.TabPanel({
 			activeTab: 0,
+			autoTabs:false,
+			border:false,
+			bodyBorder : false,
+			deferredRender: false,
+			listeners: {
+                tabchange: function(tabPanel, tab){
+                    console.log(tabPanel.id + ' : ' + tab.id);
+					if(tab.id == 'generic_stats'){
+						Ext.Ajax.request({
+							url :  '/Stats.action?currentSubscriberDistribution=&',
+							scope : this,
+							success : function(response, opts) {
+										var pg = response.responseText;
+										var resp = eval( '('+pg+')' );
+										var ctx = document.getElementById("genericStats").getContext("2d");
+										window.chart = new Chart(ctx).Pie(resp.data, {
+											responsive : true,
+											tooltipTemplate : "<%if (label){%> <%=label%> Service : <%}%> <%=Math.round(value)%> subscribers",
+											scaleBeginAtZero: false,
+											scaleBackdropPaddingY: 15,
+											scaleBackdropPaddingX: 15,
+											scaleShowLine: false,
+											segmentStrokeColor: "#fff",
+											legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\">&nbsp;&nbsp;&nbsp;</span>&nbsp;<%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+										});
+										var legend = window.chart.generateLegend();
+										var legend_Div = Ext.get("legend_Div");
+										legend_Div.update(legend);
+									},
+							failure : function() {
+									Ext.Msg.alert('Failed complete action!');
+							}
+						});
+					}
+				}
+            },
 			items: [{
 			    id : 'billing_stats',
 				title: 'Billing Stats',
@@ -43,8 +79,57 @@ function populate(){
 					html: '<div style="width: 98%; height: 86%; padding:5px;"> <canvas id="canvas" height="410" width="590"></canvas></div>'
 				}]
 			},{
-				title: 'Tab 2',
-				html: ''
+				id : 'generic_stats',
+				title: 'Traffic Statistics',
+				width:840,
+				height:550,
+				layout:'border',
+				items : [
+				
+					{
+						region:'west',
+						id:'statsLinkPanel',
+						title:'Statistics',
+						split:true,
+						fill: false,
+						width: 150,
+						minSize: 10,
+						maxSize: 15,
+						minheight: 50,
+						height: 50,
+						collapsible: true,
+						margins:'15 0 5 5',
+						cmargins:'15 5 5 5',
+						layout:'accordion',
+						layoutConfig:{
+							type: 'accordion',
+							titleCollapse: true,
+							animate: true,
+							activeOnTop: true
+						},
+						items: []
+					},{
+						region:'center',
+						margins:'15 5 5 0',
+						layout:'column',
+						autoScroll:true,
+						items:[{
+							columnWidth: .99,
+							baseCls:'x-plain',
+							bodyStyle:'padding:5px 0 5px 5px',
+							items:[{
+								id: 'statscanvas',
+								title: 'Subscription Distribution',
+								items : [
+										{
+											html:'<br/><div style="width: 98%; height: 86%; padding:5px;"> <canvas id="genericStats" ></canvas><div id="legend_Div"></div></div><br/>'
+										}
+										]
+								}]
+							}]
+					}
+				
+				]
 				
 				
 			}]
