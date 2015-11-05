099e167883e67a0ff3aa4171066249631f9d5af5
Timothy Mwangi <timomwa@gmail.com>
2015-11-05 21:09:40 +0300

    Commit 2

--- src/main/java/com/pixelandtag/cmp/ejb/api/sms/ProcessorResolverEJBImpl.java
+++ src/main/java/com/pixelandtag/cmp/ejb/api/sms/ProcessorResolverEJBImpl.java
@@ -279,7 +279,7 @@ public class ProcessorResolverEJBImpl implements ProcessorResolverEJBI {
 					+ "osms.smsservice.price_point_keyword  "//5
 				+ "FROM OpcoSMSService osms"
 				+ " WHERE osms.moprocessor.shortcode=:shortcode "
-				+ " AND osms.moprocessor.mop.enable=1 "
+				+ " AND osms.moprocessor.enable=1 "
 				+ " AND osms.smsservice.enabled=1 "
 				+ " AND osms.smsservice.cmd=:keyword"
 				+ " AND osms.opco=:opco");
@@ -303,7 +303,7 @@ public class ProcessorResolverEJBImpl implements ProcessorResolverEJBI {
 					+ "osms.smsservice.price_point_keyword  "//5
 				+ "FROM OpcoSMSService osms"
 				+ " WHERE osms.moprocessor.shortcode=:shortcode "
-				+ " AND osms.moprocessor.mop.enable=1 "
+				+ " AND osms.moprocessor.enable=1 "
 				+ " AND osms.smsservice.enabled=1 "
 				+ " AND osms.smsservice.cmd=:keyword"
 				+ " AND osms.opco=:opco");
