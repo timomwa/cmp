ecc71e7233aeef63002d28e24dc7907d673c0494
Timothy Mwangi (Admin) <timomwa@gmail.com>
2016-04-20 02:33:29 +0300

    Cache the configs using a stateful EJB

--- src/main/java/com/pixelandtag/cmp/ejb/api/billing/BillingGatewayEJBImpl.java
+++ src/main/java/com/pixelandtag/cmp/ejb/api/billing/BillingGatewayEJBImpl.java
@@ -1,9 +1,11 @@
 package com.pixelandtag.cmp.ejb.api.billing;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.ejb.EJB;
 import javax.ejb.Remote;
+import javax.ejb.Stateful;
 import javax.ejb.Stateless;
 
 import org.apache.log4j.Logger;
@@ -23,9 +25,11 @@ import com.pixelandtag.smssenders.Sender;
 import com.pixelandtag.smssenders.SenderFactory;
 import com.pixelandtag.smssenders.SenderResp;
 
-@Stateless
+@Stateful
 @Remote
 public class BillingGatewayEJBImpl implements BillingGatewayEJBI {
+	
+	private Map<Long, Biller> biller_cache = new HashMap<Long, Biller>();
 	
 	@EJB
 	private BillerConfigsI billerConfigEJB;
@@ -35,35 +39,47 @@ public class BillingGatewayEJBImpl implements BillingGatewayEJBI {
 	@Override
 	public SenderResp bill(Billable billable) throws BillingGatewayException{
 		
+		
 		if(billable==null)
 			throw new BillingGatewayException("Billable object passed is null!");
 		
 		OperatorCountry opco = billable.getOpco();
-		
+	
 		if(opco==null)
 			throw new BillingGatewayException("No opco linked with this billable!");
+	
+		Biller biller = biller_cache.get(opco.getId());
 		
-		OpcoBillingProfile billerprofile = billerConfigEJB.getActiveBillerProfile(opco);
-		
-		if(billerprofile==null)
-			throw new BillingGatewayException("No opco billing profile for opco with id "+opco.getId()
-					+". Please insert a record in the table opco_biller_profile");
-		
-		Map<String,BillerProfileConfig> opcoconfigs = billerConfigEJB.getAllConfigs(billerprofile);
-		Map<String,BillerProfileTemplate> opcotemplates = billerConfigEJB.getAllTemplates(billerprofile,TemplateType.PAYLOAD);
-		
-		BillingConfigSet billerconfigs = new BillingConfigSet();
-		billerconfigs.setOpcoconfigs(opcoconfigs);
-		billerconfigs.setOpcotemplates(opcotemplates);
+		if(biller==null){
+			
+			OpcoBillingProfile billerprofile = billerConfigEJB.getActiveBillerProfile(opco);
+			
+			if(billerprofile==null)
+				throw new BillingGatewayException("No opco billing profile for opco with id "+opco.getId()
+						+". Please insert a record in the table opco_biller_profile");
+			
+			Map<String,BillerProfileConfig> opcoconfigs = billerConfigEJB.getAllConfigs(billerprofile);
+			Map<String,BillerProfileTemplate> opcotemplates = billerConfigEJB.getAllTemplates(billerprofile,TemplateType.PAYLOAD);
+			
+			BillingConfigSet billerconfigs = new BillingConfigSet();
+			billerconfigs.setOpcoconfigs(opcoconfigs);
+			billerconfigs.setOpcotemplates(opcotemplates);
+			try {
+				biller = BillerFactory.getInstance(billerconfigs);
+				biller.validateMandatory();//Validates mandatory configs.
+			}catch (Exception exp) {
+				logger.error(exp.getMessage(),exp);
+				throw new BillingGatewayException("Problem occurred instantiating sender. Error: "+exp.getMessage());
+			}
+			
+		}
 		
 		try {
-			Biller biller = BillerFactory.getInstance(billerconfigs);
-			biller.validateMandatory();//Validates mandatory configs.
 			SenderResp resp = biller.charge(billable);
 			return resp;
 		} catch (Exception exp) {
 			logger.error(exp.getMessage(),exp);
-			throw new BillingGatewayException("Problem occurred instantiating sender. Error: "+exp.getMessage()+" Do you have entries in biller_profile_configs having profile_id_fk =  "+billerprofile.getProfile().getId());
+			throw new BillingGatewayException("Problem occurred instantiating sender. Error: "+exp.getMessage());
 		}
 	}
 
