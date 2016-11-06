10552deae14cbbcce342ef874d33aafe0e9ea7eb
Timothy Mwangi <timomwa@gmail.com>
2016-11-06 11:29:20 +0300

    Logging loggin

--- src/ussdTag.sql
+++ src/ussdTag.sql
@@ -188,15 +188,10 @@ DELIMITER ;
 
 select msisdn,convert_tz(mo_timestamp,'-04:00','+03:00') mo_time, convert_tz(mt_timestamp,'-04:00','+03:00') mt_time,shortcode,source,replace(mo_sms,'\n',' ') mo_sms, replace(mt_sms,'\n',' '), status from message_log where msisdn='254756121523' order by mo_timestamp desc;
 
+-- 79497102 Airtel --
+-- 79497164 Orange --
 select date(convert_tz(timeStamp,'-04:00','+03:00')) dt, count(*) count, sum(price) total_kshs from  success_billing where success=1 and opco_id_fk = 79497102  
-and timeStamp between  convert_tz('2016-02-29 00:00:00','-04:00','+03:00') AND convert_tz('2016-04-02 23:59:59','-04:00','+03:00')
-group by dt order by dt desc limit 35;
-
-
-
-
-select date(convert_tz(timeStamp,'-04:00','+03:00')) dt, count(*) count, sum(price) total_kshs from  success_billing where success=1  opco_id_fk = 79497102 
-and timeStamp between  convert_tz('2016-03-29 00:00:00','-04:00','+03:00') AND convert_tz('2016-04-02 23:59:59','-04:00','+03:00')
+and timeStamp between  convert_tz('2016-05-29 00:00:00','-04:00','+03:00') AND convert_tz('2016-07-02 23:59:59','-04:00','+03:00')
 group by dt order by dt desc limit 35;
 
 
