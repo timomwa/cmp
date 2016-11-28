6fdd0806d8b39c459c074e4679f3ff97fb40cebd
Timothy Mwangi <timomwa@gmail.com>
2016-11-28 13:06:52 +0300

    There there

--- src/main/java/com/pixelandtag/api/bulksms/Configuration.java
+++ src/main/java/com/pixelandtag/api/bulksms/Configuration.java
@@ -6,16 +6,16 @@ import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-//@ApplicationPath("/rest")
-public class Configuration{// extends Application {
+@ApplicationPath("/redcrossmt")
+public class Configuration extends Application {
 
 	public Configuration() {
 	}
 
-	//@Override
+	@Override
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> classes = new HashSet<Class<?>>();
-		classes.add(BulkSMSMTReceiverI.class);
+		//classes.add(BulkSMSMTReceiverI.class);
 		classes.add(BulkSMSMTReceiverImpl.class);
 		return classes;
 	}
